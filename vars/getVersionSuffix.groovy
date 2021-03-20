eb171d1e226479705947d5e9d49164e90a813ab2
fr4nlearning <francis.informatica@gmail.com>
2021-03-20 12:31:45 +0100

    new getVerssionSuffix

---
+++ vars/getVersionSuffix.groovy
@@ -0,0 +1,10 @@
+def call(Map config){
+    node{            
+        if(config.isReleaseCandidate) {
+            return config.rcNumber
+        }else{
+            return config.rcNumber+ '+ci'+env.BUILD_NUMBER
+        }
+    }
+}
+
