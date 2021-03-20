f1bd8d4f8fd18d0cc81317216f4c83bf9c76e787
fr4nlearning <francis.informatica@gmail.com>
2021-03-20 12:09:30 +0100

    added new autditTools2.groovy

---
+++ vars/auditTools2.groovy
@@ -0,0 +1,11 @@
+def call(Map config){
+    node {
+        echo ${config.message}
+        sh '''
+        git version
+        docker version
+        node --version
+        npm version
+        '''
+    }
+}
